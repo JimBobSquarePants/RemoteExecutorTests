a82ed6f20ab8125807eb891219f155459b93febc
James Jackson-South <james_south@hotmail.com>
2020-10-10 01:19:40 +0100

    Attempt to get error message from CI

--- ExecutorTest/UnitTest1.cs
+++ ExecutorTest/UnitTest1.cs
@@ -1,18 +1,40 @@
 using Microsoft.DotNet.RemoteExecutor;
+using System;
+using System.Diagnostics;
+using System.IO;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace ExecutorTest
 {
     public class UnitTest1
     {
+        private readonly ITestOutputHelper output;
+
+        public UnitTest1(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
         [Fact]
         public void ShouldThrow()
         {
+            var tracePath = Path.Combine(Path.GetTempPath(), $"remoteexecutor{DateTime.Now:yyyyMMdd}.txt");
             Assert.True(RemoteExecutor.IsSupported);
 
+            var psi = new ProcessStartInfo();
+            psi.Environment[$"COREHOST_TRACE"] = "1";
+            psi.Environment[$"COREHOST_TRACEFILE"] = tracePath;
+
             Assert.Throws<RemoteExecutionException>(() =>
-                RemoteExecutor.Invoke(() => Assert.True(false)).Dispose()
+                RemoteExecutor.Invoke(() => Assert.True(false),
+                 new RemoteInvokeOptions
+                 {
+                     StartInfo = psi
+                 }).Dispose()
             );
+
+            output.WriteLine(File.ReadAllText(tracePath));
         }
 
         [Fact]
